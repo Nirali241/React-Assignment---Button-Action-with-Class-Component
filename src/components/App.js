78e72d10ec69e85788cae8354bf08efaeb441d44
Nirali Bhalodiya <75874828+Nirali241@users.noreply.github.com>
2021-02-15 16:39:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			render:false,
+		};
 	};
+
+	handleClick=()=>{
+		this.setState({
+			render:true,
+		});
+	};
+	
+
+	renderPara=()=>{
+		if(this.state.render){
+			return (
+				<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+			);
+		}
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+    
+        <button id="click" onClick={this.handleClick}>Click</button>
+				{ this.renderPara() }
     		</div>
     	);
     }
@@ -17,4 +37,3 @@ class App extends Component {
 
 
 export default App;
-
